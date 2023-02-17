3af524f9f547d251ec91bd306ac0db66029c7b85
aidas agejevas <aidas.agejevas@gmail.com>
2023-02-17 22:45:34 +0200

    moved scraped documents to database

--- Web/DodoPizza.js
+++ Web/DodoPizza.js
@@ -1,4 +1,5 @@
 const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+const axios = require('axios');
 require('chromedriver');
 
 async function scrape() {
@@ -13,8 +14,21 @@ async function scrape() {
     const title = await deal.findElement(By.css('.title')).getText();
     const description = await deal.findElement(By.css('.description')).getText();
     
-    //...
+    const data = {
+      image: image,
+      name: title,
+      description: description,
+      price: '3gay',
+      restaurantID: 1
+    };
+
+    axios.post('http://localhost:3000/api/meal-deals/', data)
+      .catch(error => {
+        console.log(error)
+      })
   }
 
   driver.quit();
 }
+
+scrape()
--- Web/DominosPizza.js
+++ Web/DominosPizza.js
@@ -1,10 +1,14 @@
 const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+const axios = require('axios')
 require('chromedriver');
 
 async function scrape() {
   let driver = await new Builder().forBrowser('chrome').build();
 
   await driver.get('https://dominospizza.lt/lt/vilnius/promo/');
+
+  var button = await driver.findElement(By.css(".CookieAccept"))    
+  button.click()
 
   var dealCards = await driver.findElements(By.css(".promo-card__content"));
   
@@ -13,8 +17,21 @@ async function scrape() {
     const title = await deal.findElement(By.css('.card-title')).getText();
     const description = await deal.findElement(By.css('.card-text')).getText();
 
-    //...
+    const data = {
+      image: image,
+      name: title,
+      description: description,
+      price: '3gay',
+      restaurantID: 2
+    };
+
+    axios.post('http://localhost:3000/api/meal-deals/', data)
+      .catch(error => {
+        console.log(error)
+      })
   }
 
   driver.quit();
 }
+
+scrape()
